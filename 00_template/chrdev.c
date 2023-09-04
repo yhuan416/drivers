97317a4e50e2bcc227ec03588b5707ee4c1cde58
yhuan416 <yhuan416@foxmail.com>
2023-09-04 23:06:30 +0800

    feat: add template

---
+++ 00_template/chrdev.c
@@ -0,0 +1,23 @@
+#include <linux/types.h>
+#include <linux/kernel.h>
+#include <linux/delay.h>
+#include <linux/gpio.h>
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int __init chrdev_init(void)
+{
+    printk("chrdev_init\n");
+    return 0;
+}
+
+static void __exit chrdev_exit(void)
+{
+    printk("chrdev_exit\n");
+}
+
+module_init(chrdev_init);
+module_exit(chrdev_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("yhuan416 <yhuan416@foxmail.com>");
